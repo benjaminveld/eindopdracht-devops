1dc1b31a3bafae13e1cd76f5caf3719a46cabbf4
jurrelegein <jurrelegein12@gmail.com>
2024-03-08 02:09:21 +0100

    classes aangepast naar DTO en concept toegevoegd voor favorites

--- main.py
+++ main.py
@@ -2,35 +2,48 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-from models import User, Gender, Role
+from models import UserDTO, GenderDTO, RoleDTO, FavoriteDTO
 from uuid import UUID, uuid4
 
 app = FastAPI()
 
-db: List[User] = [
-    User(
+userdb: List[UserDTO] = [
+    UserDTO(
         id=uuid4(),
         first_name="Dev",
         last_name="Ops",
-        gender=Gender.other,
+        gender=GenderDTO.other,
         age=21,
-        role=Role.student
+        role=RoleDTO.student
     ),
-    User(
+    UserDTO(
         id=uuid4(),
         first_name="Ops",
         last_name="Dev",
-        gender=Gender.other,
+        gender=GenderDTO.other,
         age=44,
-        role=Role.working
+        role=RoleDTO.working
     )
+]
+
+favoritedb: List[FavoriteDTO]= [
+
 ]
 
 @app.get("/api/v1/users")
 async def fetch_users():
-    return db;
+    return userdb;
 
 @app.post("/api/v1/users")
-async def register_user(user: User):
-    db.append(user)
-    return{"id": user.id}
+async def register_user(user: UserDTO):
+    userdb.append(user)
+    return{"id": user.id}\
+
+@app.get("/api/v1/favorites")
+async def fetch_favorite():
+    return favoritedb
+
+@app.post("/api/v1/favorites")
+async def register_favorites(favorite: FavoriteDTO):
+    favoritedb.append(favorite)
+    return{"favorite": favorite.currency}
