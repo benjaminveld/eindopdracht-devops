7b896fd7e5d4392857e899f4dd9c0cd39b98a1c0
Jurrelegein <jurrelegein12@gmail.com>
2024-03-25 15:07:53 +0100

    Poging 1 van statuscode toevoegen aan log

--- middleware.py
+++ middleware.py
@@ -4,9 +4,15 @@ from apilogger import logger
 async def log_middleware(request: Request, call_next):
     log_dict= {
         "url": request.url.path,
-        "method": request.method
+        "method": request.method,
     }
     logger.info(log_dict)
 
     response = await call_next(request)
+
+    log_dict["response_code"] = response.status_code
+
+    logger.info(log_dict)
+
     return response
+
